672bef2815b5262421bc7f0abc7eb44f4cd25f53
Pedro Costa Nunes <111301318+pedrocostanunes@users.noreply.github.com>
2023-04-20 12:22:15 -0300

    Update main.py

--- API/main.py
+++ API/main.py
@@ -3,8 +3,8 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-one_hot_enc = pd.read_pickle('one_hot_encoder.pkl')
 modelo = pd.read_pickle('modelo_treinado.pkl')
+one_hot_enc = pd.read_pickle('one_hot_encoder.pkl')
 scaler = pd.read_pickle('scaler.pkl')
 
 @app.get('/modelo/v1={idade}&v2={salario}&v3={situacao_moradia}&v4={tempo_trabalho}&v5={motivo_emprestimo}&v6={pontuacao_emprestimo}&v7={valor_emprestimo}&v8={taxa_juros}&v9={renda_percentual_emprestimo}&v10={devendo}&v11={tempo_de_credito}')
@@ -15,10 +15,10 @@ def previsao_modelo(idade, salario, situacao_moradia, tempo_trabalho,
     dados = {
         'idade': [float(idade)],
         'salario': [float(salario)],
-        'situacao_moradia': [situacao_moradia],
+        'situacao_moradia': [(situacao_moradia)],
         'tempo_trabalho': [float(tempo_trabalho)],
-        'motivo_emprestimo': [motivo_emprestimo],
-        'pontuacao_emprestimo': [pontuacao_emprestimo],
+        'motivo_emprestimo': [(motivo_emprestimo)],
+        'pontuacao_emprestimo': [(pontuacao_emprestimo)],
         'valor_emprestimo': [float(valor_emprestimo)],
         'taxa_juros': [float(taxa_juros)],
         'renda_percentual_emprestimo': [float(renda_percentual_emprestimo)],
@@ -27,10 +27,12 @@ def previsao_modelo(idade, salario, situacao_moradia, tempo_trabalho,
     }
 
     dados = pd.DataFrame(dados)
+
     dados = one_hot_enc.transform(dados)
     dados_transformados = pd.DataFrame(dados, columns=one_hot_enc.get_feature_names_out())
+
     dados_transformados = scaler.transform(dados_transformados)
     dados_transformados = pd.DataFrame(dados_transformados, columns = one_hot_enc.get_feature_names_out())
-    return {'result': modelo.predict(dados_transformados)[0],
-            'probability_0': modelo.predict_proba(dados_transformados).tolist()[0][0],
-            'probability_1': modelo.predict_proba(dados_transformados).tolist()[0][1]}
+    return {'result': float(modelo.predict(dados_transformados)[0]),
+            'probability_0': float(modelo.predict_proba(dados_transformados).tolist()[0][0]),
+            'probability_1': float(modelo.predict_proba(dados_transformados).tolist()[0][1])}
